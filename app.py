8f0ddc495153972ec2db2507dae4f149997bac03
jonathan <uuupah@gmail.com>
2019-07-29 17:14:16 +0930

    format update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from tkinter import * # pylint: disable=unused-wildcard-import
+from tkinter import *  # pylint: disable=unused-wildcard-import
 import spritemap
 import dungeon_generation
 from PIL import Image, ImageTk
@@ -12,6 +12,7 @@ grid_size = (canvas_size-1)/grid_count
 spritemap = spritemap.SpriteMap('spritemap/0/', 37)
 tilemap = None
 
+
 def main():
     print("Running...")
 
@@ -21,15 +22,18 @@ def main():
     root.winfo_toplevel().title("DNDungeonWorldGenerator")
 
     # Title label
-    title = Label(root, anchor=CENTER, text="DNDungeonWorldGenerator", pady=5, font=("Helvetica", 30))
+    title = Label(root, anchor=CENTER, text="DNDungeonWorldGenerator",
+                  pady=5, font=("Helvetica", 30))
     title.pack()
 
     # Map canvas
-    canvas = Canvas(root, height=canvas_size, width=canvas_size, bg="white", relief="solid", borderwidth=canvas_border_size)
+    canvas = Canvas(root, height=canvas_size, width=canvas_size,
+                    bg="white", relief="solid", borderwidth=canvas_border_size)
     canvas.pack()
     canvas.update()
 
-    generate_button = Button(root, text="Generate", command=lambda: generate_map(canvas))
+    generate_button = Button(root, text="Generate",
+                             command=lambda: generate_map(canvas))
     generate_button.pack()
 
     init_grid(canvas)
@@ -37,23 +41,29 @@ def main():
     mainloop()
 
 # Initialize the grid lines
+
+
 def init_grid(canvas):
     print("Initializing Grid...")
 
     # Instantiate the underlying grid lines
     for y in range(grid_count):
         for x in range(grid_count):
-            canvas.create_rectangle(x*grid_size+2, 
-                                    y*grid_size+2, 
-                                    (x+1)*grid_size+2, 
-                                    (y+1)*grid_size+2, 
+            canvas.create_rectangle(x*grid_size+2,
+                                    y*grid_size+2,
+                                    (x+1)*grid_size+2,
+                                    (y+1)*grid_size+2,
                                     outline="black", width=1, tags="gridline")
 
 # Call this after you add anything to the canvas to raise the grid lines back to the top
+
+
 def reset_grid(canvas):
     canvas.tag_raise("gridline")
 
 # Generates a tilemap from the spritemap
+
+
 def generate_map(canvas):
     print("Generating map...")
     global tilemap
@@ -62,17 +72,24 @@ def generate_map(canvas):
     reset_grid(canvas)
 
 # Displays the tilemap on the canvas
+
+
 def display_tilemap(canvas, tilemap):
     print("Displaying map...")
     for y in range(grid_count):
         for x in range(grid_count):
-            image = Image.open(tilemap.spritemap.sprites[tilemap.tiles[y * grid_count + x].sprite].filepath)
-            image = image.resize((math.ceil(grid_size), math.ceil(grid_size)), Image.NEAREST)
-            tilemap.tiles[y * grid_count + x].photoimg = ImageTk.PhotoImage(image)
+            image = Image.open(
+                tilemap.spritemap.sprites[tilemap.tiles[y * grid_count + x].sprite].filepath)
+            image = image.resize(
+                (math.ceil(grid_size), math.ceil(grid_size)), Image.NEAREST)
+            tilemap.tiles[y * grid_count +
+                          x].photoimg = ImageTk.PhotoImage(image)
 
-            canvas.create_image(x*grid_size+2+grid_size/2, 
+            canvas.create_image(x*grid_size+2+grid_size/2,
                                 y*grid_size+2+grid_size/2,
-                                image=tilemap.tiles[y * grid_count + x].photoimg
+                                image=tilemap.tiles[y *
+                                                    grid_count + x].photoimg
                                 )
+
 
 main()
--- dungeon_generation.py
+++ dungeon_generation.py
@@ -1,15 +1,17 @@
 import spritemap
 import random
 
+
 class Tile:
-    def __init__ (self, x, y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
         self.decided = False
         self.sprite = None
 
+
 class TileMap:
-    def __init__ (self, tile_count, spritemap):
+    def __init__(self, tile_count, spritemap):
         self.width = tile_count
         self.height = tile_count
         self.tiles = list(())
@@ -18,14 +20,15 @@ class TileMap:
         for y in range(tile_count):
             for x in range(tile_count):
                 self.tiles.append(Tile(x, y))
-    
-    def get_tile (self, x, y):
+
+    def get_tile(self, x, y):
         if(x < self.width and x >= 0 and y < self.width and y >= 0):
             return self.tiles[y * self.width + x]
         else:
             return None
 
-def get_decided (value, tiles):
+
+def get_decided(value, tiles):
     output = list(())
 
     for tile in tiles:
@@ -34,11 +37,12 @@ def get_decided (value, tiles):
 
     return output
 
-def get_possible_placements (tilemap):
+
+def get_possible_placements(tilemap):
     possible_placements = list(())
 
     for tile in get_decided(False, tilemap.tiles):
-        #Check each side
+        # Check each side
         right_tile = tilemap.get_tile(tile.x+1, tile.y)
         left_tile = tilemap.get_tile(tile.x-1, tile.y)
         up_tile = tilemap.get_tile(tile.x, tile.y+1)
@@ -58,44 +62,48 @@ def get_possible_placements (tilemap):
             up_possibilities = tilemap.spritemap.sprites[up_tile.sprite].down
         else:
             up_possibilities = range(0, tilemap.spritemap.sprite_count)
-            
+
         if down_tile != None and down_tile.decided == True:
             down_possibilities = tilemap.spritemap.sprites[down_tile.sprite].up
         else:
             down_possibilities = range(0, tilemap.spritemap.sprite_count)
 
-        possible_placements_tile = list(set(right_possibilities) & set(left_possibilities) & set(up_possibilities) & set(down_possibilities))
+        possible_placements_tile = list(set(right_possibilities) & set(
+            left_possibilities) & set(up_possibilities) & set(down_possibilities))
 
         if len(possible_placements_tile) > 0:
             possible_placements.append([tile, possible_placements_tile])
-    
+
     return possible_placements
 
-def set_tile_neighbors_undecided (tilemap, tile):
+
+def set_tile_neighbors_undecided(tilemap, tile):
     for x in range(-1, 2):
         for y in range(-1, 2):
             temp_tile = tilemap.get_tile(tile.x + x, tile.y + y)
             if temp_tile != None:
                 temp_tile.decided = False
 
+
 def generate_tilemap(size, spritemap):
     tilemap = TileMap(size, spritemap)
     while len(get_decided(False, tilemap.tiles)) != 0:
-        #Check if valid tile can be placed
+        # Check if valid tile can be placed
         possible = get_possible_placements(tilemap)
         if len(possible) > 0:
-            #If it can select a random one
+            # If it can select a random one
             tile_chosen = random.randint(0, len(possible))-1
             sprite_index = random.randint(0, len(possible[tile_chosen][1]))-1
             sprite_chosen = possible[tile_chosen][1][sprite_index]
-            
-            #place it
+
+            # place it
             possible[tile_chosen][0].sprite = sprite_chosen
             possible[tile_chosen][0].decided = True
-        #OTHERWISE we select a random undecided tile and set its neighbors to undecided
+        # OTHERWISE we select a random undecided tile and set its neighbors to undecided
         else:
             undecided_tiles = get_decided(False, tilemap.tiles)
             undecided_index = random.randint(0, len(undecided_tiles))-1
-            set_tile_neighbors_undecided(tilemap, undecided_tiles[undecided_index])
+            set_tile_neighbors_undecided(
+                tilemap, undecided_tiles[undecided_index])
 
     return tilemap
